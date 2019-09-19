e97f5d0c1b3bd8d0cb7e677e36eb0cc2b020d590
fwcd <fwcdmail@gmail.com>
2019-09-19 05:01:55 +0200

    Add doc comments to the game structures

--- src/game.rs
+++ src/game.rs
@@ -4,29 +4,38 @@
 use std::collections::HashMap;
 use crate::xml_node::XmlNode;
 
+/// A message indicating that the client
+/// has joined a room with the specified id.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Joined {
 	pub room_id: String
 }
 
+/// A message in a room together with some data.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Room {
 	pub room_id: String,
 	pub data: Data
 }
 
+/// Either a welcome message or a
+/// game state wrapped in a memento.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Data {
 	WelcomeMessage { color: PlayerColor },
 	Memento { state: GameState }
 }
 
+/// A player color in the game.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PlayerColor {
 	Red,
 	Blue
 }
 
+/// A snapshot of the game's state at
+/// a specific turn. Consists of the
+/// board and information about both players.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct GameState {
 	pub turn: u32,
@@ -37,12 +46,15 @@ pub struct GameState {
 	pub board: Board
 }
 
+/// Axial coordinates on the hex grid.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct AxialCoords {
 	pub x: i32,
 	pub y: i32
 }
 
+/// Cube coordinates on the hex grid.
+/// These are used by the protocol internally.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CubeCoords {
 	pub x: i32,
@@ -50,6 +62,8 @@ pub struct CubeCoords {
 	pub z: i32
 }
 
+/// The game board which is a symmetric hex grid with
+/// a side length of 6 fields.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Board {
 	// TODO: Store fields contiguously in a Vec
@@ -57,6 +71,7 @@ pub struct Board {
 	pub fields: HashMap<AxialCoords, Field>
 }
 
+/// A field on the game board.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Field {
 	pub is_obstructed: bool
