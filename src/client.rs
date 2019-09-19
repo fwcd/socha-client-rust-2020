6a6da093cec9155ffa0e6daea213bc9d06fdefac
fwcd <fwcdmail@gmail.com>
2019-09-19 02:55:57 +0200

    Setup logging and TCP connection

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,7 @@
+use std::net::TcpStream;
+use std::io::{self, Read, Write};
+use log::info;
+
 /// A handler that implements the game player's
 /// behavior, usually employing some custom move
 /// selection strategy.
@@ -8,12 +12,28 @@ pub trait SCClientDelegate {
 /// The client which handles XML requests, manages
 /// the game state and invokes the delegate.
 pub struct SCClient<D> {
-	delegate: D
+	delegate: D,
+	debug_enabled: bool
 }
 
 impl<D> SCClient<D> {
 	/// Creates a new client using the specified delegate.
-	pub fn new(delegate: D) -> Self {
-		Self { delegate: delegate }
+	pub fn new(delegate: D, debug_enabled: bool) -> Self {
+		Self { delegate: delegate, debug_enabled: debug_enabled }
+	}
+	
+	/// Blocks the thread and begins reading XML messages
+	/// from the provided address via TCP.
+	pub fn run(self, host: &str, port: u16) -> io::Result<()> {
+		let address = format!("{}:{}", host, port);
+		let mut stream = TcpStream::connect(&address)?;
+		info!("Connected to {}", address);
+		
+		if self.debug_enabled {
+			// In debug mode, only the XML messages will be output
+			io::copy(&mut stream, &mut io::stdout())?;
+		}
+		
+		Ok(())
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod client;
 mod logic;
 
 use std::env;
+use std::str::FromStr;
+use simplelog::{SimpleLogger, Config};
+use log::LevelFilter;
 use getopts::Options;
 use client::SCClient;
 use logic::OwnGameLogic;
@@ -18,6 +21,8 @@ fn main() {
     options.optopt("h", "host", "The game server's host address", "HOST");
     options.optopt("p", "port", "The game server's port", "PORT");
     options.optopt("r", "reservation", "A game reservation", "RESERVATION");
+    options.optopt("l", "level", "Optionally provides a custom log level ('Info' by default)", "LEVEL");
+    options.optflag("d", "debug", "Prints all incoming XML messages to the console for debugging");
     options.optflag("H", "help", "Prints usage info");
     
     let parsed_args = options.parse(&args[1..]).expect("Could not parse arguments!");
@@ -25,11 +30,18 @@ fn main() {
         print_usage(&args[0], options);
         return;
     }
-
+    
     let host = parsed_args.opt_str("host").unwrap_or("localhost".to_owned());
-    let port = parsed_args.opt_str("port").unwrap_or("13050".to_owned());
+    let port = parsed_args.opt_str("port").unwrap_or("13050".to_owned()).parse::<u16>().expect("Invalid port.");
     let reservation = parsed_args.opt_str("reservation").unwrap_or("".to_owned());
+    let level = parsed_args.opt_str("level").unwrap_or("Info".to_owned());
+    
+    // Setup logging
+    SimpleLogger::init(LevelFilter::from_str(&level).expect("Invalid log level."), Config::default()).expect("Could not initialize logger.");
     
     // Setup the client and the delegate
-    let client = SCClient::new(OwnGameLogic);
+    let debug_enabled = parsed_args.opt_present("debug");
+    let client = SCClient::new(OwnGameLogic, debug_enabled);
+    
+    client.run(&host, port).expect("Error while running client.");
 }
